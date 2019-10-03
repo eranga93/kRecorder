45f0cbcf7b061fa0cf6b0a69b39e93a9221beacc
eranga93 <eranga@duosoftware.com>
2019-10-03 16:05:24 +0530

    added files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import ExpressRecorder from './ExpressRecorder';
+
+function App() {
+  return (
+    <div className="App">
+     <ExpressRecorder></ExpressRecorder>
+    </div>
+  );
+}
+
+export default App;
